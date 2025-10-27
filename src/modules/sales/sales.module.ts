f8bcbd2410ab62b6ddcf9c36bb3556c9eea86329
MellyASD <melissaramirezortegon@hotmail.com>
2025-10-27 17:52:18 -0500

    Modulo sales

--- src/modules/sales/sales.module.ts
+++ src/modules/sales/sales.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SalesService } from './sales.service';
 import { SalesController } from './sales.controller';
-import { Sale } from './entities/sales.entity';
-import { SaleDetail } from './entities/sale-detail.entity';
+import { Sale } from 'src/entities/sales.entity';
+import { SaleDetail } from 'src/entities/sale-detail.entity';
 import { Product } from 'src/entities/products.entity';
 import { User } from 'src/entities/user.entity';
 
